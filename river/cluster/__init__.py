f46f0a784442e5aa3c4e055367a3ecc0b978ed3e
Kiên Nguyễn <trungkienlucky@gmail.com>
2026-09-09 10:21:37 +0700

    feat(cluster): add HPStream clustering algorithm

--- river/cluster/__init__.py
+++ river/cluster/__init__.py
@@ -4,10 +4,22 @@ from __future__ import annotations
 
 from .clustream import CluStream
 from .dbstream import DBSTREAM
+from .dcustream import DCUStream
 from .denstream import DenStream
+from .hpstream import HPStream
 from .k_means import KMeans
 from .odac import ODAC
 from .streamkmeans import STREAMKMeans
 from .textclust import TextClust
 
-__all__ = ["CluStream", "DBSTREAM", "DenStream", "KMeans", "ODAC", "STREAMKMeans", "TextClust"]
+__all__ = [
+    "CluStream",
+    "DBSTREAM",
+    "DCUStream",
+    "DenStream",
+    "HPStream",
+    "KMeans",
+    "ODAC",
+    "STREAMKMeans",
+    "TextClust",
+]
